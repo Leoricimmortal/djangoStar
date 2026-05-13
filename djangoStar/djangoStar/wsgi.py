487184f95c26f5ade2fab7ec1ad3fa8ba837fc33
RICARDO CASTRO DE MATOS <ricardocastrokdev@gmail.com>
2026-05-13 00:04:03 -0300

    Add files via upload

---
+++ djangoStar/djangoStar/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for djangoStar project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangoStar.settings')
+
+application = get_wsgi_application()
---
+++ djangoStar/starapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StarappConfig(AppConfig):
+    name = 'starapp'
